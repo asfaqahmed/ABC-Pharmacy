e1eae6737ce5be8c1018faef107ad0f093552308
Asfaq Ahmed <asfaqahmed356@gmail.com>
2024-03-11 23:45:27 +0530

    Add files via upload
    Static page added

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
